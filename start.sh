94cebba51c3fa1ac6dd7bc6ff6b8e4fc930b838b
Bernhard Enders <bgeneto@users.noreply.github.com>
2022-07-14 11:11:50 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
-sudo mount -t tmpfs -o size=4G tmpfs /mnt/ramdisk 
-sudo mkdir -p /mnt/ramdisk/mariadb/data       
-sudo mkdir -p /mnt/ramdisk/mariadb/config       
-sudo cp ./server.cnf /mnt/ramdisk/mariadb/config/server.cnf
+#!/bin/sh
+
+# requires sudo to create directories and ramdisk
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root (e.g. with sudo)"
+  exit
+fi
+mount -t tmpfs -o size=4G tmpfs /mnt/ramdisk 
+mkdir -p /mnt/ramdisk/mariadb/data       
+mkdir -p /mnt/ramdisk/mariadb/config       
+cp ./server.cnf /mnt/ramdisk/mariadb/config/server.cnf
 docker compose up -d
